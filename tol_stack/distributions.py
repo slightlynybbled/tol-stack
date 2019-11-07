b32b39d809541d1316e33db0ae72cc816cb9f52f
Jason R. Jones <slightlynybbled@gmail.com>
2019-11-07 07:09:10 -0500

    Adding limits to the number of times that the distribution is allowed to be calculated to eliminate possibility of entering into an infinite loop.

--- tol_stack/distributions.py
+++ tol_stack/distributions.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+_max_iterations = 100
+
 
 def norm(loc, scale, size=1):
     return np.random.normal(loc=loc, scale=scale, size=size)
@@ -13,10 +15,14 @@ def norm_screened(loc, scale, limits: tuple, size=1):
 
         # removes values not in range
         values = values[(values >= low_limit) & (values <= high_limit)]
-
+        count = 0
         while len(values) < size:
             values = np.append(values, np.random.normal(loc=loc, scale=scale, size=size))
             values = values[(values >= low_limit) & (values <= high_limit)]
+            count += 1
+            if count > _max_iterations:
+                raise ValueError('number of iterations exceeds the max allowable... are the limits set appropriately?')
+
         values = values[:size]
 
     return values
@@ -31,9 +37,15 @@ def norm_notched(loc, scale, limits: tuple, size=1):
         # removes values not in range
         values = values[(values <= low_limit) | (values >= high_limit)]
 
+        count = 0
         while len(values) < size:
             values = np.append(values, np.random.normal(loc=loc, scale=scale, size=size))
             values = values[(values <= low_limit) | (values >= high_limit)]
+
+            count += 1
+            if count > _max_iterations:
+                raise ValueError('number of iterations exceeds the max allowable... are the limits set appropriately?')
+
         values = values[:size]
 
     return values
@@ -45,9 +57,15 @@ def norm_lt(loc, scale, limit, size=1):
     # removes values not in range
     values = values[values <= limit]
 
+    count = 0
     while len(values) < size:
         values = np.append(values, np.random.normal(loc=loc, scale=scale, size=size))
         values = values[(values <= limit)]
+
+        count += 1
+        if count > _max_iterations:
+            raise ValueError('number of iterations exceeds the max allowable... is the limit set appropriately?')
+
     values = values[:size]
 
     return values
@@ -59,9 +77,15 @@ def norm_gt(loc, scale, limit, size=1):
     # removes values not in range
     values = values[values >= limit]
 
+    count = 0
     while len(values) < size:
         values = np.append(values, np.random.normal(loc=loc, scale=scale, size=size))
         values = values[(values >= limit)]
+
+        count += 1
+        if count > _max_iterations:
+            raise ValueError('number of iterations exceeds the max allowable... is the limit set appropriately?')
+
     values = values[:size]
 
     return values
