39d7995df42a3c7cbedf37d5ae46b102e3bc45c8
Jason R. Jones <slightlynybbled@gmail.com>
2019-11-11 10:32:39 -0500

    Adding "on change" behavior to the stackup frame on the GUI.

--- tol_stack/gui/app.py
+++ tol_stack/gui/app.py
@@ -33,10 +33,11 @@ class TitleFrame(BaseFrame):
 
 
 class StackupFrame(BaseFrame):
-    def __init__(self, parent, loglevel=logging.INFO):
+    def __init__(self, parent, on_change_callback: callable = None, loglevel=logging.INFO):
         super().__init__(parent=parent, loglevel=loglevel)
 
         path_types = StackPath.retrieve_stackup_path_types()
+        self._on_change_callback = on_change_callback
 
         r = 0
         tk.Label(self, text='Stackup', font=self.font_heading)\
@@ -45,11 +46,31 @@ class StackupFrame(BaseFrame):
         r += 1
         self._stack_type_var = tk.StringVar()
         self._stack_type_var.set(path_types[0])
-        #self._stack_type_var.trace('w', callback=self._on_path_type_change)
+        self._stack_type_var.trace('w', callback=self._on_change)
 
         tk.Label(self, text='Select Stack Type:').grid(row=r, column=0, sticky='w')
         self._stack_type_cb = ttk.Combobox(self, textvariable=self._stack_type_var, values=path_types)
         self._stack_type_cb.grid(row=r, column=1, sticky='ew')
+
+        r += 1
+        tk.Label(self, text='Samples', font=self.font).grid(row=r, column=0, sticky='e')
+        self._size_entry = tk.Entry(self)
+        self._size_entry.grid(row=r, column=1, sticky='ew')
+        self._size_entry.insert(0, '100000')
+
+        self._size_entry.bind('<Return>', self._on_change)
+
+    def _on_change(self, *args):
+        path_type = self._stack_type_var.get()
+        size = int(self._size_entry.get())
+
+        self._logger.info(f'Data changed; path_type="{path_type}", size={size}')
+
+        if self._on_change_callback is not None:
+            self._on_change_callback(
+                path_type=path_type,
+                size=size
+            )
 
 
 class PartDefinitionsFrame(BaseFrame):
